cbf92c336bcae16293b541f6df7a19b2f66f0477
irishcarbomb777 <danielclannan@gmail.com>
2023-07-28 15:50:03 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+
+
+export abstract class Command {
+    abstract returnType: any
+    constructor(public wrappedFn: <C extends any>(client: C) => any) {}
+}
+
+
+export abstract class CommandClient {
+    send = async <T extends Command>(command: T): Promise<T['returnType']> => {
+        try {
+            return command.wrappedFn(this)
+        } catch (error) {
+            throw error
+        }
+    }
+}
